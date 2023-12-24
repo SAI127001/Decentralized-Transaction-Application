893e9ca13973d586512f07e839b53e37e514c71f
SAI127001 <codesai127.0.0.1@gmail.com>
2023-12-24 12:37:16 +0530

    Dec 24 Modify

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import {saket, sai, ether} from "../assets"
-import { SiEthereum} from "react-icons/si";
+import { saket, sai, ether } from "../assets";
 
 const About = ({ color, title, image, subtitle1, subtitle2 }) => (
   <div className="flex flex-row justify-start items-start white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
-    <div className={`w-10 h-10 rounded-full flex justify-center items-center ${color}`}>
+    <div
+      className={`w-10 h-10 rounded-full flex justify-center items-center ${color} about`}
+    >
       {image ? (
-        <img src={image} alt={`Image of ${title}`} className="w-10 h-10 rounded-full" />
+        <img
+          src={image}
+          alt={`Image of ${title}`}
+          className="w-10 h-10 about-img rounded-full"
+        />
       ) : null}
     </div>
     <div className="ml-5 flex flex-col flex-1">
       <h3 className="mt-2 text-white text-lg">{title}</h3>
       <p className="mt-2 text-white text-sm md:w-9/12">
         {subtitle1}
-        <p>
-        {subtitle2}
-        </p>
+        <p>{subtitle2}</p>
       </p>
     </div>
   </div>
@@ -29,27 +32,28 @@ const AboutCard = () => (
           Name of the College, Student and Project.
         </h1>
         <p className="text-left my-2 text-white font-light md:w-9/12 w-11/12 text-base">
-        The purpose of this crypto exchange project is to enable the exchange of Ethereum,
-         offering users a platform to transfer their digital assets securely and conveniently.
+          The purpose of this crypto exchange project is to enable the exchange
+          of Ethereum, offering users a platform to transfer their digital
+          assets securely and conveniently.
         </p>
       </div>
 
       <div className="flex-1 flex flex-col justify-start items-center">
         <About
-          color="black"
+          color=""
           title="SAKET COLLEGE OF ARTS, SCIENCE & COMMERCE"
           image={saket}
           subtitle=""
         />
         <About
-          color="bg-[#8945F8]"
+          color=""
           title="TERUKULA SAI"
           image={sai}
-          subtitle1="PNR No : 2021016400730531" 
+          subtitle1="PNR No : 2021016400730531"
           subtitle2="Roll No : 233026 "
         />
         <About
-          color="black"
+          color=""
           title="Decentralized Transaction Web3 Application"
           image={ether}
           subtitle=""
