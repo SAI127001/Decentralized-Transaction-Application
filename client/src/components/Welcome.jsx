6cb592a2e73d22922cab31b699380a82ba457fb2
SAI127001 <codesai127.0.0.1@gmail.com>
2024-01-09 17:11:58 +0530

    Jan 9 modification

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -18,10 +18,10 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
   <input
     placeholder={placeholder}
     type={type}
-    step="0.0001"
+    step="0.000001"
     value={value}
     onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-[1px] border-[#c8a849] text-sm white-glassmorphism"
   />
 );
 
